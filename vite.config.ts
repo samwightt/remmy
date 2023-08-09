9649a3fa9c406d1a8b7ab6e859c0492da225725d
Sam Wight <me@samw.dev>
2023-08-08 19:31:59 -0500

    Fix Vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { replaceCodePlugin } from 'vite-plugin-replace'
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), replaceCodePlugin({
+      replacements: [
+        {
+          from: 'const form_data_1 = __importDefault(require("form-data"));',
+          to: ""
+        },
+        {
+          from: "form_data_1.default",
+          to: "FormData"
+        }
+      ]
+    })],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
